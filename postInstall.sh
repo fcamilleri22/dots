ca8a368afde9916b47373fcb640e16a8cc89d5b0
Frank Camilleri <fcamilleri22@gmail.com>
2018-07-06 12:40:12 -0400

    Initial Commit.

---
+++ postInstall.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/bash
+
+#Update Repos/get nearest repos -- omit ibiblio, is slow.
+sudo pacman-mirrors -c United_States
+sudo sed -i '/ibiblio/d' /etc/pacman.d/mirrorlist
+
+#Edit default /etc/pacman.conf (purely aesthetics)
+sudo sed -i '/Color/s/^#//'           /etc/pacman.conf
+sudo sed -i '/TotalDownload/s/^#//'   /etc/pacman.conf
+sudo sed -i '/VerbosePkgLists/s/^#//' /etc/pacman.conf
+
+#Edit default /etc/nanorc:
+sudo sed -i '/set autoindent/s/^#//'                            /etc/nanorc
+sudo sed -i '/set boldtext/s/^#//'                              /etc/nanorc
+sudo sed -i '/set mouse/s/^#//'                                 /etc/nanorc
+sudo sed -i '/set linenumbers/s/^#//'                           /etc/nanorc
+sudo sed -i '/set tabstospaces/s/^#//'                          /etc/nanorc
+
+sudo sed -i '/set tabsize 8/s/8$/4/'                            /etc/nanorc
+sudo sed -i '/set tabsize 4/s/^#//'                             /etc/nanorc
+
+sudo sed -i '/include \"\/usr\/share\/nano\/\*.nanorc\"/s/^#//' /etc/nanorc
+
+#Update already installed packages/refresh caches
+sudo pacman -Syyu
+
+#Get non-aur stuff
+sudo pacman -S                                                                  \
+    base-devel                                                                  \
+    yaourt                                                                      \
+    mariadb                                                                     \
+    mysql-workbench                                                             \
+    nodejs                                                                      \
+    npm                                                                         \
+    zsh                                                                         \
+    atom                                                                        \
+    git                                                                         \
+    apm	                                                                        \
+    firefox-developer-edition                                                   \
+    python-pywal                                                                \
+    polybar                                                                     \
+    nerd-fonts-terminus                                                         \
+    adobe-source-code-pro-fonts                                                 \
+    adobe-source-sans-pro-fonts                                                 \
+    adobe-source-serif-pro-fonts                                                \
+
+#Update package caches/Install Packages from Arch User Repository
+yaourt -Syu
+
+yaourt -S                                                                       \
+    smartgit                                                                    \
+    oh-my-zsh-git                                                               \
+    nerd-fonts-fira-code                                                        \
+
+#Set up MariaDB
+sudo mysql_install_db                                                           \
+    --user=mysql                                                                \
+    --basedir=/usr                                                              \
+    --datadir=/var/lib/mysql                                                    \
+
+sudo systemctl start mysqld
+mysql_secure_installation
+
+#Atom plugins
+#TODO: Fill as you go along.
+apm install gruvbox-plus-syntax
+
+#TODO: Get Settings/Backups from git
+#i3
+#zsh
+#xinitrc
+
+
+#TODO: ZSH configs
+#oh-my-zsh from the AUR ends up in /usr/share/oh-my-zsh
+#Keep your ZSHRC portable!
+
+#TODO: Configure Pywal, polybar
+#Add
+
+#TODO: and then rebuild this using more minimal iso, possibly just straight up arch
+
+#TODO: Alter firefox fonts in ~/.mozilla/firefox/*.default
