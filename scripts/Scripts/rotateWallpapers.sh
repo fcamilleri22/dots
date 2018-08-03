679b78c00bf17ed111654bdebd74ea8c385a1c9d
Frank Camilleri <fcamilleri22@gmail.com>
2018-08-03 15:50:14 -0400

    Add: Description Boilerplate on rotateWallpaper script

--- scripts/Scripts/rotateWallpapers.sh
+++ scripts/Scripts/rotateWallpapers.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+################################################################################
+#Author: Frank Camilleri (http://deployfrank.sh)
+#TL;DR: rotates wallpapers from a predefined directory
+#License: MIT (do w/e you want with this code as long as MIT license copy
+#included, and I'm not responsible if this code breaks your stuff)
+################################################################################
+
 WALLPAPERDIR=$HOME/Pictures/FranksWallpaperCollection
 
 while true
