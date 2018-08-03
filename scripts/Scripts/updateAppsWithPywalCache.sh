c3e650b7f6d231636ab82782b12322e56a84fb6a
Frank Camilleri <fcamilleri22@gmail.com>
2018-08-03 15:51:12 -0400

    Add: post pywal script for pushing cached configs to their desired points. all boilerplate for now

---
+++ scripts/Scripts/updateAppsWithPywalCache.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+################################################################################
+#Author: Frank Camilleri (http://deployfrank.sh)
+#TL;DR: Takes themed config files from the pywal cache and puts them where they
+#need to be. Run with pywal using 'wal -o ...'
+#License: MIT (do w/e you want with this code as long as MIT license copy
+#included, and I'm not responsible if this code breaks your stuff)
+################################################################################
+
+#Atom Theme
+
+#Dunst Theme
