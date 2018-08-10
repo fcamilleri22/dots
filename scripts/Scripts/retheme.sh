18ade9fa042dc3a0fe3612ff536ceae73233aa2d
Frank Camilleri <fcamilleri22@gmail.com>
2018-08-10 13:01:14 -0400

    Change: retheme.sh is fully fleshed out. yuss.

--- scripts/Scripts/retheme.sh
+++ scripts/Scripts/retheme.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 ################################################################################
 #Author: Frank Camilleri (http://deployfrank.sh)
-#TL;DR: Takes themed config files from the pywal cache and puts them where they
-#need to be. Run with pywal using 'wal -o ...'
+#TL;DR: a wrapper for pywal to simplify retheming.
 #License: MIT (do w/e you want with this code as long as MIT license copy
 #included, and I'm not responsible if this code breaks your stuff)
 ################################################################################
 
+wal -g -a 85 --saturate 0.9 -n $@
+
+#don't continue if error in wal
+[ $? -ne 0 ] && exit 1
+
 #Atom Theme
-mv \
+cp \
     $HOME/.cache/wal/colors-atom-syntax \
     $HOME/.atom/packages/frank-syntax/styles/colors.less
 
 #Dunst (notification daemon) Theme
-mv \
+cp \
     $HOME/.cache/wal/colors-dunst \
     $HOME/.config/dunst/dunstrc
+
+echo "Some programs may need to be restarted for changes to take effect."
