dcea8ccf117c4b278deef6ee6585cc54160457a1
Frank Camilleri <fcamilleri22@gmail.com>
2018-08-28 15:29:08 -0400

    Add: a good solid working update script.

---
+++ scripts/Scripts/updateDots.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+################################################################################
+#Author: Frank Camilleri (http://deployfrank.sh)
+#TL;DR: run this after pulling a new version of dots.
+#License: MIT (do w/e you want with this code as long as MIT license copy
+#included, and I'm not responsible if this code breaks your stuff)
+################################################################################
+
+#Determine if root or 'nobody'. If either, exit script.
+if [ $UID -eq 0 ] || [ $UID -eq 99 ]
+then
+    echo "ERROR: This script should be invoked by the primary user, and definitely not root. Exiting."
+    exit 1
+fi
+
+#Set where we're looking for the (possibly updated) install script.
+SCRIPTDIR=$1
+[ -z "$SCRIPTDIR" ] && SCRIPTDIR=$HOME/Projects/dots/postInstall.sh
+
+#Determine if postInstall.sh exists where we want it, or exit.
+if [ ! -e $SCRIPTDIR ]
+then
+    echo "$SCRIPTDIR doesn't exist! Exiting."
+    exit 1
+fi
+
+#Rerun section 2 of the install script.
+#This includes a full system update.
+sh -c "$(cat $SCRIPTDIR | sed -n "/#2.)/,/#3.)/p")"
+
+#Then, handle config files.
+$HOME/Scripts/stowConfigs.sh $(dirname $SCRIPTDIR)
